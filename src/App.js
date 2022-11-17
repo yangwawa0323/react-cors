c294994a1c428acf0493925ad0d67c70cf79656c
yangwawa0323 <yangwawa0323@163.com>
2022-11-17 12:53:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField'
+import axios from 'axios';
+import { ArrowForward } from '@mui/icons-material';
+import { Button } from '@mui/material'
 
 function App() {
+  const [value, setValue] = React.useState('')
+  const [responseData, setResponseData] = React.useState(null)
+  const [error, setError] = React.useState(null)
+
+  const postData = async () => {
+    await axios.post(`${process.env.REACT_APP_API_SERVER}/api/page/new`, {
+      content: value,
+    }, {
+      Headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        setResponseData(response.data)
+      })
+      .catch(err => {
+        console.log("[DEBUG]:", err)
+        setError(err);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField
+          id=""
+          label="Post content"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+
+        />
+        <Button onClick={postData} variant="outlined" color="primary" endIcon={<ArrowForward />}>
+          Post
+        </Button>
+        <div className="output">
+          <h6>
+            Response :
+          </h6>
+          <p>
+            {responseData}
+          </p>
+          <h6>Error: </h6>
+          <p>
+            { error && JSON.stringify(error, null ,2 )}
+          </p>
+        </div>
       </header>
     </div>
   );
